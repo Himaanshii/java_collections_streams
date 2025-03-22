2a62585211887519288edbc8a2d4bafee2e8254c
Himaanshii <himanshi2157.be21@chitkara.edu.in>
2025-03-22 11:35:25 +0530

    Updated

--- list_interface/nthElement.java
+++ list_interface/nthElement.java
@@ -4,16 +4,16 @@ public class nthElement {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         LinkedList<Character> ls = new LinkedList<Character>();
-        System.out.println("Enter number of elements");
+        System.out.println("Enter number of characters");
         int n = sc.nextInt();
-        System.out.println("Enter elements");
+        System.out.println("Enter characters");
         for(int i =0; i<n; i++){
             char a = sc.next().charAt(0);
             ls.add(a);
         }
-        System.out.println("Enter number for which you want to find element");
+        System.out.println("Enter position for which you want to find element");
         int p = sc.nextInt();
-        System.out.println("Nth element from end is "+findElement(ls,p));
+        System.out.println(p+"th element from end is "+findElement(ls,p));
     }
     public static char findElement(LinkedList<Character> ls , int n){
         return ls.get(ls.size()-n);
